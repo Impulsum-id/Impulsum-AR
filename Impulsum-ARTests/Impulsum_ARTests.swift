05d23671d55a9850eba8ba5b3f74667bdcff0227
Rizki Maulana <rizki.maulana@majoo.id>
2024-10-03 15:20:34 +0700

    Initial Commit

---
+++ Impulsum-ARTests/Impulsum_ARTests.swift
@@ -0,0 +1,17 @@
+//
+//  Impulsum_ARTests.swift
+//  Impulsum-ARTests
+//
+//  Created by Rizki Maulana on 03/10/24.
+//
+
+import Testing
+@testable import Impulsum_AR
+
+struct Impulsum_ARTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
